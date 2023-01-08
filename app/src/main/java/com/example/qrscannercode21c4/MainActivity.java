6002dcbe955b586ced851e09bb564817ebbd4e4c
LENOVO <dellaananda451@gmail.com>
2023-01-07 23:19:27 -0500

    penambahan

--- app/src/main/java/com/example/qrscannercode21c4/MainActivity.java
+++ app/src/main/java/com/example/qrscannercode21c4/MainActivity.java
@@ -1,14 +1,160 @@
 package com.example.qrscannercode21c4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.qrcode.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //View object
+    private Button buttonScanning;
+    private TextView textViewName, textViewClass, textViewId;
+    private FusedLocationProviderClient locationProviderClient;
+
+    //qr code scanner object
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activitymain);
+
+        //View object
+        buttonScanning = (Button) findViewById(R.id.buttonscan);
+        textViewName = (TextView) findViewById(R.id.textViewNama);
+        textViewClass = (TextView) findViewById(R.id.textViewKelas);
+        textViewId = (TextView) findViewById(R.id.TextViewNim);
+
+        //inisialisasi scan object
+        qrScan = new IntentIntegrator(this);
+
+        //implementasi onClickListener
+        buttonScanning.setOnClickListener(this);
+
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        buttonScanning.setOnClickListener(v -> {
+            getLocation();
+        });
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 10){
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "Izin Lokasi tidak diaktifkan!", Toast.LENGTH_SHORT).show();
+            }else{
+                getLocation();
+            }
+        }
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //get permission
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, 10);
+
+           } else {
+            //get locationn
+            locationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        textViewName.setText(String.valueOf(location.getLatitude()));
+                        textViewClass.setText(String.valueOf(location.getLongitude()));
+                        textViewId.setText(String.valueOf(location.getAltitude()));
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Lokasi tidak aktif", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                 Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result != null){
+            //jika result code scanner tidak sama sekali
+            if(result.getContents()== null){
+                Toast.makeText(this,  "Hasil scan tidak ada", Toast.LENGTH_LONG).show();
+            }else if (Patterns.WEB_URL.matcher(result.getContents()).matches()) {
+                Intent visitUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(result.getContents()));
+                startActivity(visitUrl);
+            } else if (Patterns.PHONE.matcher(result.getContents()).matches()) {
+                String telp = String.valueOf(result.getContents());
+                Intent Intent = new Intent(android.content.Intent.ACTION_DIAL, Uri.parse("tel:" + telp));
+                Intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(Intent);
+                try {
+                    startActivity(Intent.createChooser(Intent, "waiting"));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(MainActivity.this, "no phone apk installed", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else{
+
+                try {
+                    JSONObject obj = new JSONObject(result.getContents());
+
+                    textViewName.setText(obj.getString("nama"));
+                    textViewClass.setText(obj.getString("kelas"));
+                    textViewId.setText(obj.getString("NIM"));
+                }catch (JSONException e){
+                    e.printStackTrace();
+                    Toast.makeText(this, result.getContents(),Toast.LENGTH_LONG).show();
+                }
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        //inisialisasi qrcode scanning
+        qrScan.initiateScan();
+
+
     }
 }
